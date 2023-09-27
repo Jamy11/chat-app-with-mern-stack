17fddee50687e6d614f19253f120210b6f8b16d2
jamy11 <jamyarrahman82@gmail.com>
2023-09-27 01:33:14 -0230

    fetch Chat api is created and tested

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const { protect } = require("../middleware/authMiddleware");
-const { accessChat } = require("../controllers/chatController");
+const { accessChat, fetchChats } = require("../controllers/chatController");
 
 const router = express.Router();
 
 router.route("/").post(protect, accessChat).get(protect, fetchChats);
-
 // router.route("/rename").put(protect, rename);
 // router.route("/groupremove").put(protect, groupRemove);
 // router.route("/groupadd").put(protect, groupAdd);

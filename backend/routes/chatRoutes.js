bf72ad36a6ff8bc8a9379156745a768dc7897e88
jamy11 <jamyarrahman82@gmail.com>
2023-09-27 23:31:24 -0230

    added chat api and fixed all routes

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const { protect } = require("../middleware/authMiddleware");
-const { accessChat, fetchChats } = require("../controllers/chatController");
+const {
+  accessChat,
+  fetchChats,
+  createGroupChat,
+  renameGroup,
+  removeFromGroup,
+  addToGroup,
+} = require("../controllers/chatController");
 
 const router = express.Router();
 
 router.route("/").post(protect, accessChat).get(protect, fetchChats);
-// router.route("/rename").put(protect, rename);
-// router.route("/groupremove").put(protect, groupRemove);
-// router.route("/groupadd").put(protect, groupAdd);
+router.route("/group").post(protect, createGroupChat);
+router.route("/rename").put(protect, renameGroup);
+router.route("/groupremove").put(protect, removeFromGroup);
+router.route("/groupadd").put(protect, addToGroup);
 module.exports = router;

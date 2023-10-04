17a8ef0426f01e7bfedbab56c8b5a20d1492f90b
jamy11 <jamyarrahman82@gmail.com>
2023-10-04 00:55:32 -0230

    Creating Chat Page

---
+++ frontend/src/components/SideDrawer.js
@@ -0,0 +1,77 @@
+import { Button } from "@chakra-ui/button";
+import { useDisclosure } from "@chakra-ui/hooks";
+import { Input } from "@chakra-ui/input";
+import { Box, Text } from "@chakra-ui/layout";
+import { Menu, MenuButton, MenuDivider, MenuItem } from "@chakra-ui/menu";
+import {
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+} from "@chakra-ui/modal";
+import { Tooltip } from "@chakra-ui/tooltip";
+import { BellIcon, ChevronDownIcon } from "@chakra-ui/icons";
+import { Avatar } from "@chakra-ui/avatar";
+import { useToast } from "@chakra-ui/toast";
+import { Spinner } from "@chakra-ui/spinner";
+import { useState } from "react";
+import { ChatState } from "../context/ChatProvider";
+
+const SideDrawer = () => {
+  const [search, setSearch] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [loadingChat, setLoadingChat] = useState(false);
+
+  const { user } = ChatState();
+  return (
+    <>
+      <Box
+        d="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        bg="white"
+        w="100%"
+        p="5px 10px 5px 10px"
+        borderWidth="5px"
+        style={{ display: "flex" }}
+      >
+        <Tooltip label="Search Users to chat" hasArrow placement="bottom-end">
+          <Button
+            variant="ghost"
+            // onClick={onOpen}
+          >
+            <i className="fas fa-search"></i>
+            <Text d={{ base: "none", md: "flex" }} px={4}>
+              Search User
+            </Text>
+          </Button>
+        </Tooltip>
+        <Text fontSize="2xl" fontFamily="Work sans">
+          Chat App Yay
+        </Text>
+        <div>
+          <Menu>
+            <MenuButton>
+              <BellIcon fontSize="2xl" m={1} />
+            </MenuButton>
+            {/* <MenuList></MenuList> */}
+          </Menu>
+          <Menu>
+            <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+              <Avatar
+                size={"sm"}
+                cursor={"pointer"}
+                name={user.name}
+                src={user.pic}
+              />
+            </MenuButton>
+          </Menu>
+        </div>
+      </Box>
+    </>
+  );
+};
+
+export default SideDrawer;
--- frontend/src/pages/private/Chats.js
+++ frontend/src/pages/private/Chats.js
@@ -1,10 +1,30 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { ChatState } from "../../context/ChatProvider";
+import { Box } from "@chakra-ui/layout";
+import SideDrawer from "../../components/SideDrawer";
+import MyChats from "../../components/MyChats";
+import Chatbox from "../../components/Chatbox";
 
 const Chats = () => {
   const { user } = ChatState();
-  return <div style={{ width: "100%" }}>{user && "asdasdsa"}</div>;
+  return (
+    <div style={{ width: "100%" }}>
+      {user && <SideDrawer />}
+      <Box d="flex" justifyContent="space-between" w="100%" h="91.5vh" p="10px">
+        {user && (
+          <MyChats
+          //  fetchAgain={fetchAgain}
+          />
+        )}
+        {user && (
+          <Chatbox
+          // fetchAgain={fetchAgain} setFetchAgain={setFetchAgain}
+          />
+        )}
+      </Box>
+    </div>
+  );
 };
 
 export default Chats;
